4615e4e714cea233eb1e4d80a5033dc0c8b3a5a6
Thomas Fletcher <tom@tom-fletcher.co.uk>
2019-08-12 21:10:11 +0100

    Order Manager View Added.

--- MyShop/MyShop.Services/OrderService.cs
+++ MyShop/MyShop.Services/OrderService.cs
@@ -34,5 +34,21 @@ namespace MyShop.Services
             orderContext.Insert(baseOrder);
             orderContext.Commit();
         }
+
+        public List<Order> GetOrderList()
+        {
+            return orderContext.Collection().ToList();
+        }
+
+        public Order GetOrder(string id)
+        {
+            return orderContext.Find(id);
+        }
+
+        public void UpdateOrder(Order updatedOrder)
+        {
+            orderContext.Update(updatedOrder);
+            orderContext.Commit();
+        }
     }
 }

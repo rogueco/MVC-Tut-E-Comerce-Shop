637e914bf036b7e1cacb8a7aebd267b098d207a9
Thomas Fletcher <tom@tom-fletcher.co.uk>
2019-08-12 18:22:38 +0100

    Added Enhanced Register Customers

--- MyShop/MyShop.WebUI/Models/AccountViewModels.cs
+++ MyShop/MyShop.WebUI/Models/AccountViewModels.cs
@@ -79,6 +79,23 @@ namespace MyShop.WebUI.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Street { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        public string County { get; set; }
+
+        [Required]
+        public string PostCode { get; set; }
     }
 
     public class ResetPasswordViewModel
